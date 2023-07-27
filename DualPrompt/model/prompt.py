759d4fca7123d9612fc26936e159092c931c2439
yunseokddi <oys5339@naver.com>
2023-07-27 18:08:21 +0900

    Prompt implementing..

--- DualPrompt/model/prompt.py
+++ DualPrompt/model/prompt.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn as nn
 
+
 class EPrompt(nn.Module):
     def __init__(self, length=5, embed_dim=768, embedding_key='mean', prompt_init='uniform', prompt_pool=False,
                  prompt_key=False, pool_size=None, top_k=None, batchwise_prompt=False, prompt_key_init='uniform',
-                 num_layers=1, use_prefix_tune_for_e_prompt=False, num_heads=-1, same_key_value=False,):
+                 num_layers=1, use_prefix_tune_for_e_prompt=False, num_heads=-1, same_key_value=False, ):
         super().__init__()
 
         self.length = length
@@ -52,7 +53,6 @@ class EPrompt(nn.Module):
                     self.prompt = nn.Parameter(torch.randn(prompt_pool_shape))
                     nn.init.uniform_(self.prompt, -1, 1)
 
-
         if prompt_key:
             key_shape = (pool_size, embed_dim)
 
@@ -61,10 +61,10 @@ class EPrompt(nn.Module):
 
             elif prompt_key_init == 'uniform':
                 self.prompt_key = nn.Parameter(torch.randn(key_shape))
-                nn.init.uniform_(self.prompt_key, -1,1)
+                nn.init.uniform_(self.prompt_key, -1, 1)
 
         else:
-            prompt_mean = torch.mean(self.prompt, dim=[0,2])
+            prompt_mean = torch.mean(self.prompt, dim=[0, 2])
             self.prompt_key = prompt_mean
 
     def l2_normalize(self, x, dim=None, epsilon=1e-12):
@@ -76,4 +76,46 @@ class EPrompt(nn.Module):
     def forward(self, x_embed, prompt_mask=None, cls_features=None):
         out = dict()
 
+        if self.prompt_pool:
+            if self.embedding_key == 'mean':
+                x_embed_mean = torch.mean(x_embed, dim=1)
+            elif self.embedding_key == 'max':
+                x_embed_mean = torch.max(x_embed, dim=1)[0]
+            elif self.embedding_key == 'mean_max':
+                x_embed_mean = torch.max(x_embed, dim=1)[0] + 2 * torch.mean(x_embed, dim=1)
+            elif self.embedding_key == 'cls':
+                if cls_features is None:
+                    x_embed_mean = torch.max(x_embed, dim=1)[0]  # B, C
+                else:
+                    x_embed_mean = cls_features
+            else:
+                raise NotImplementedError("Not supported way of calculating embedding keys!")
 
+            prompt_key_norm = self.l2_normalize(self.prompt_key, dim=-1)  # pool_size, C
+            x_embed_norm = self.l2_normalize(x_embed_mean, dim=-1)  # B, C
+
+            similarity = torch.matmul(prompt_key_norm, x_embed_norm.t())  # pool_size, B or pool_size, # class, B
+            similarity = similarity.t()  # B, pool_size
+
+            (similarity_top_k, idx) = torch.topk(similarity, k=self.top_k, dim=1)  # B, top_k
+            out['similarity'] = similarity
+
+            if self.batchwise_prompt:
+                prompt_id, id_counts = torch.unique(idx, return_counts=True, sorted=True)
+
+                if prompt_id.shape[0] < self.pool_size:
+                    prompt_id = torch.cat([prompt_id,
+                                           torch.full((self.pool_size - prompt_id.shape[0],), torch.min(idx.flatten()),
+                                                      device=prompt_id.device)])
+                    id_counts = torch.cat(
+                        [id_counts, torch.full((self.pool_size - id_counts.shape[0],), 0, device=id_counts.device)])
+
+                _, major_idx = torch.topk(id_counts, k=self.top_k)
+                major_prompt_id = prompt_id[major_idx]
+
+                idx = major_prompt_id.expand(x_embed.shape[0], -1).contiguous() # B, top_k
+
+            if prompt_mask is not None:
+                idx =  prompt_mask # B, top_k
+
+            
