028458aaac721fa4a946858a5924fb01b94a3151
yunseokddi <oys5339@naver.com>
2023-07-29 18:10:54 +0900

    Trainer start

--- DualPrompt/model/prompt.py
+++ DualPrompt/model/prompt.py
@@ -22,6 +22,7 @@ class EPrompt(nn.Module):
         self.same_key_value = same_key_value
 
         if self.prompt_pool:
+            # user prefix style
             if self.use_prefix_tune_for_e_prompt:
                 assert embed_dim % self.num_heads == 0
                 if self.same_key_value:
@@ -34,7 +35,6 @@ class EPrompt(nn.Module):
                         self.prompt = nn.Parameter(torch.randn(prompt_pool_shape))
                         nn.init.uniform_(self.prompt, -1, 1)
                     self.prompt = self.prompt.repeat(1, 2, 1, 1, 1, 1)
-
                 else:
                     prompt_pool_shape = (self.num_layers, 2, self.pool_size, self.length,
                                          self.num_heads, embed_dim // self.num_heads)
@@ -44,7 +44,6 @@ class EPrompt(nn.Module):
                         self.prompt = nn.Parameter(torch.randn(
                             prompt_pool_shape))  # num_layers, 2, pool_size, length, num_heads, embed_dim // num_heads
                         nn.init.uniform_(self.prompt, -1, 1)
-
             else:
                 prompt_pool_shape = (self.num_layers, self.pool_size, self.length, embed_dim)
                 if prompt_init == 'zero':
@@ -53,17 +52,17 @@ class EPrompt(nn.Module):
                     self.prompt = nn.Parameter(torch.randn(prompt_pool_shape))
                     nn.init.uniform_(self.prompt, -1, 1)
 
+        # if using learnable prompt keys
         if prompt_key:
             key_shape = (pool_size, embed_dim)
-
             if prompt_key_init == 'zero':
                 self.prompt_key = nn.Parameter(torch.zeros(key_shape))
-
             elif prompt_key_init == 'uniform':
                 self.prompt_key = nn.Parameter(torch.randn(key_shape))
                 nn.init.uniform_(self.prompt_key, -1, 1)
-
         else:
+            # else use mean of prompt as key
+            # only compatible with prompt, not prefix
             prompt_mean = torch.mean(self.prompt, dim=[0, 2])
             self.prompt_key = prompt_mean
 
@@ -75,7 +74,6 @@ class EPrompt(nn.Module):
 
     def forward(self, x_embed, prompt_mask=None, cls_features=None):
         out = dict()
-
         if self.prompt_pool:
             if self.embedding_key == 'mean':
                 x_embed_mean = torch.mean(x_embed, dim=1)
@@ -91,10 +89,10 @@ class EPrompt(nn.Module):
             else:
                 raise NotImplementedError("Not supported way of calculating embedding keys!")
 
-            prompt_key_norm = self.l2_normalize(self.prompt_key, dim=-1)  # pool_size, C
+            prompt_key_norm = self.l2_normalize(self.prompt_key, dim=-1)  # Pool_size, C
             x_embed_norm = self.l2_normalize(x_embed_mean, dim=-1)  # B, C
 
-            similarity = torch.matmul(prompt_key_norm, x_embed_norm.t())  # pool_size, B or pool_size, # class, B
+            similarity = torch.matmul(prompt_key_norm, x_embed_norm.t())  # pool_size, B or Pool_size, #class, B
             similarity = similarity.t()  # B, pool_size
 
             (similarity_top_k, idx) = torch.topk(similarity, k=self.top_k, dim=1)  # B, top_k
@@ -102,20 +100,81 @@ class EPrompt(nn.Module):
 
             if self.batchwise_prompt:
                 prompt_id, id_counts = torch.unique(idx, return_counts=True, sorted=True)
-
+                # In jnp.unique, when the 'size' is specified and there are fewer than the indicated number of elements,
+                # the remaining elements will be filled with 'fill_value', the default is the minimum value along the specified dimension.
+                # Unless dimension is specified, this will be flattend if it is not already 1D.
                 if prompt_id.shape[0] < self.pool_size:
                     prompt_id = torch.cat([prompt_id,
                                            torch.full((self.pool_size - prompt_id.shape[0],), torch.min(idx.flatten()),
                                                       device=prompt_id.device)])
                     id_counts = torch.cat(
                         [id_counts, torch.full((self.pool_size - id_counts.shape[0],), 0, device=id_counts.device)])
-
-                _, major_idx = torch.topk(id_counts, k=self.top_k)
-                major_prompt_id = prompt_id[major_idx]
-
-                idx = major_prompt_id.expand(x_embed.shape[0], -1).contiguous() # B, top_k
+                _, major_idx = torch.topk(id_counts, k=self.top_k)  # top_k
+                major_prompt_id = prompt_id[major_idx]  # top_k
+                # expand to batch
+                idx = major_prompt_id.expand(x_embed.shape[0], -1).contiguous()  # B, top_k
 
             if prompt_mask is not None:
-                idx =  prompt_mask # B, top_k
+                idx = prompt_mask  # B, top_k
 
-            
+            out['prompt_idx'] = idx
+            if self.use_prefix_tune_for_e_prompt:
+                batched_prompt_raw = self.prompt[:, :, idx]  # num_layers, B, top_k, length, C
+                num_layers, dual, batch_size, top_k, length, num_heads, heads_embed_dim = batched_prompt_raw.shape
+                batched_prompt = batched_prompt_raw.reshape(
+                    num_layers, batch_size, dual, top_k * length, num_heads, heads_embed_dim
+                )
+            else:
+                batched_prompt_raw = self.prompt[:, idx]
+                num_layers, batch_size, top_k, length, embed_dim = batched_prompt_raw.shape
+                batched_prompt = batched_prompt_raw.reshape(
+                    num_layers, batch_size, top_k * length, embed_dim
+                )
+
+            batched_key_norm = prompt_key_norm[idx]  # B, top_k, C
+
+            out['selected_key'] = batched_key_norm
+            out['prompt_key_norm'] = prompt_key_norm
+            out['x_embed_norm'] = x_embed_norm
+
+            # Put pull_constraint loss calculation inside
+            x_embed_norm = x_embed_norm.unsqueeze(1)  # B, 1, C
+            sim = batched_key_norm * x_embed_norm  # B, top_k, C
+            reduce_sim = torch.sum(sim) / x_embed.shape[0]  # Scalar
+
+            out['reduce_sim'] = reduce_sim
+        else:
+            # user prefix style
+            if self.use_prefix_tune_for_e_prompt:
+                assert embed_dim % self.num_heads == 0
+                if self.same_key_value:
+                    prompt_pool_shape = (self.num_layers, 1, self.length,
+                                         self.num_heads, embed_dim // self.num_heads)
+                    if self.prompt_init == 'zero':
+                        self.prompt = nn.Parameter(torch.zeros(prompt_pool_shape))
+                    elif self.prompt_init == 'uniform':
+                        self.prompt = nn.Parameter(torch.randn(prompt_pool_shape))
+                        nn.init.uniform_(self.prompt, -1, 1)
+                    self.prompt = self.prompt.repeat(1, 2, 1, 1, 1)
+                else:
+                    prompt_pool_shape = (self.num_layers, 2, self.length,
+                                         self.num_heads, embed_dim // self.num_heads)
+                    if self.prompt_init == 'zero':
+                        self.prompt = nn.Parameter(torch.zeros(prompt_pool_shape))
+                    elif self.prompt_init == 'uniform':
+                        self.prompt = nn.Parameter(
+                            torch.randn(prompt_pool_shape))  # num_layers, 2, length, num_heads, embed_dim // num_heads
+                        nn.init.uniform_(self.prompt, -1, 1)
+                batched_prompt = self.prompt.unsqueeze(0).expand(-1, x_embed.shape[0], -1, -1, -1)
+            else:
+                prompt_pool_shape = (self.num_layers, self.length, embed_dim)
+                if self.prompt_init == 'zero':
+                    self.prompt = nn.Parameter(torch.zeros(prompt_pool_shape))
+                elif self.prompt_init == 'uniform':
+                    self.prompt = nn.Parameter(torch.randn(prompt_pool_shape))
+                    nn.init.uniform_(self.prompt, -1, 1)
+                batched_prompt = self.prompt.unsqueeze(0).expand(-1, x_embed.shape[0], -1, -1)
+
+        out['batched_prompt'] = batched_prompt
+
+        return out
--- DualPrompt/train.py
+++ DualPrompt/train.py
@@ -5,13 +5,15 @@ import torch
 import torch.backends.cudnn
 import random
 import numpy as np
-
+import model.model
 
 from parse_config import CIFAR100_get_args_parser
 from pathlib import Path
 from utils.utils import init_distributed_mode
 from data_loader.data_loaders import ContinualDataLoader
 from timm.models import create_model
+from timm.scheduler import create_scheduler
+from timm.optim import create_optimizer
 
 warnings.filterwarnings('ignore')
 
@@ -41,6 +43,82 @@ def main(args):
         drop_path_rate=args.drop_path,
         drop_block_rate=None,
     )
+
+    print(f"Creating model: {args.model}")
+
+    model = create_model(
+        args.model,
+        pretrained=args.pretrained,
+        num_classes=args.nb_classes,
+        drop_rate=args.drop,
+        drop_path_rate=args.drop_path,
+        drop_block_rate=None,
+        prompt_length=args.length,
+        embedding_key=args.embedding_key,
+        prompt_init=args.prompt_key_init,
+        prompt_pool=args.prompt_pool,
+        prompt_key=args.prompt_key,
+        pool_size=args.size,
+        top_k=args.top_k,
+        batchwise_prompt=args.batchwise_prompt,
+        prompt_key_init=args.prompt_key_init,
+        head_type=args.head_type,
+        use_prompt_mask=args.use_prompt_mask,
+        use_g_prompt=args.use_g_prompt,
+        g_prompt_length=args.g_prompt_length,
+        g_prompt_layer_idx=args.g_prompt_layer_idx,
+        use_prefix_tune_for_g_prompt=args.use_prefix_tune_for_g_prompt,
+        use_e_prompt=args.use_e_prompt,
+        e_prompt_layer_idx=args.e_prompt_layer_idx,
+        use_prefix_tune_for_e_prompt=args.use_prefix_tune_for_e_prompt,
+        same_key_value=args.same_key_value,
+    )
+
+    original_model.to(device)
+    model.to(device)
+
+    if args.freeze:
+        # all parameters are frozen for original vit model
+        for p in original_model.parameters():
+            p.requires_grad = False
+
+        # freeze args.freeze[blocks, patch_embed, cls_token] parameters
+        for n, p in model.named_parameters():
+            if n.startswith(tuple(args.freeze)):
+                p.requires_grad = False
+
+    print(args)
+
+    model_without_ddp = model
+
+    if args.distributed:
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
+        model_without_ddp = model.module
+
+    n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print('number of params: {}'.format(n_parameters))
+
+    if args.unscale_lr:
+        global_batch_size = args.batch_size
+    else:
+        global_batch_size = args.batch_size * args.world_size
+
+    args.lr = args.lr * global_batch_size / 256.0
+
+    optimizer = create_optimizer(args, model_without_ddp)
+
+    if args.sched != 'constant':
+        lr_scheduler, _ = create_scheduler(args, optimizer)
+
+    elif args.sched == 'constant':
+        lr_sheduler = None
+
+    criterion = torch.nn.CrossEntropyLoss().to(device)
+
+    
+
+
+
 
 
 if __name__ == "__main__":
