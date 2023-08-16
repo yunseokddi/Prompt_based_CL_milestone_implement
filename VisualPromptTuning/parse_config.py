fb192bb25906d92fb98eab46d00231a2ff9bd639
yunseokddi <oys5339@naver.com>
2023-08-16 17:32:48 +0900

    VPT start

---
+++ VisualPromptTuning/parse_config.py
@@ -0,0 +1,25 @@
+def get_args_parser(subparsers):
+    # subparsers.add_argument('--', default=, type=, help='')
+    subparsers.add_argument('--batch-size', default=24, type=int, help='Batch size per device')
+    subparsers.add_argument('--epochs', default=50, type=int)
+
+    # Model parameters
+    subparsers.add_argument('--model', default='vit_base_patch16_224', type=str, metavar='MODEL',
+                            help='Name of model to train')
+
+    subparsers.add_argument('--DBG', default=False, type=bool, help='Debugging mode')
+
+    # distributed training parameters
+    subparsers.add_argument('--world_size', default=1, type=int,
+                            help='number of distributed processes')
+    subparsers.add_argument('--dist_url', default='env://', help='url used to set up distributed training')
+
+    # Data parameters
+    subparsers.add_argument('--dataset', default='CUB', type=str, help='dataset name')
+    subparsers.add_argument('--output_dir', default='./output', type=str, help='Model result dir')
+    subparsers.add_argument('--device', default='cuda', help='device to use for training / testing')
+    subparsers.add_argument('--seed', default=42, type=int)
+    subparsers.add_argument('--gpu', default=[2,3], type=list)
+
+    # Tensorboard
+    subparsers.add_argument('--tensorboard', default=False, type=bool)
---
+++ VisualPromptTuning/train.py
@@ -0,0 +1,60 @@
+import torch
+import torch.backends.cudnn as cudnn
+import warnings
+import argparse
+import numpy as np
+import random
+
+from parse_config import get_args_parser
+from pathlib import Path
+from tensorboard_logger import configure
+from utils.utils import init_distributed_mode
+
+warnings.filterwarnings('ignore')
+
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = True
+
+
+def main(args):
+    init_distributed_mode(args)
+
+    device = torch.device(args.device)
+
+    seed = args.seed
+
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+    print("finish")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser('VPT training and evaluation configs')
+    config = parser.parse_known_args()[-1][0]
+
+    subparser = parser.add_subparsers(dest='subparser_name')
+
+    if config == "CUB_VPT":
+        config_parser = subparser.add_parser('CUB_VPT', help='CUB2000 Visual Prompt Tuning')
+        get_args_parser(config_parser)
+
+    args = parser.parse_args()
+
+    if args.output_dir:
+        Path(args.output_dir).mkdir(parents=True, exist_ok=True)
+
+    if args.tensorboard:
+        configure("runs/%s" % (args.dataset))
+
+    main(args)
+
+'''
+CUDA_VISIBLE_DEVICES=3 python -m torch.distributed.launch \
+        --nproc_per_node=1 \
+        --use_env train.py \
+        CUB_VPT \
+        --model vit_base_patch16_224 \
+        --batch-size 128 \
+        --tensorboard True
+'''
